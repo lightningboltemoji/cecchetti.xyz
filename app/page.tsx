c94c1d2a974679739eb3b22766227902b324c6e2
Tanner <51724788+lightningboltemoji@users.noreply.github.com>
2023-06-16 20:11:16 -0700

    Add word cloud spiral effect in the background

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,28 @@
 import { faGithubSquare, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { faSquareEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Spiral from "./spiral";
 
 export default function Home() {
   return (
-    <div className="flex flex-col w-screen h-screen justify-center">
+    <div className="flex flex-col w-screen h-screen justify-center overflow-hidden">
       <div className="flex flex-col items-center">
-        <video className="w-48 rounded-full mb-2" autoPlay loop muted playsInline controls={false}>
-          <source src="memoji.mp4" type="video/mp4" />
-          There should be a memoji here, but it looks like your browser doesn&apos;t support it!
-        </video>
+        <div className="grid grid-rows-1 grid-cols-1">
+          <video
+            className="w-48 rounded-full mb-2 row-span-full col-span-full"
+            autoPlay
+            loop
+            muted
+            playsInline
+            controls={false}
+          >
+            <source src="memoji.mp4" type="video/mp4" />
+            There should be a memoji here, but it looks like your browser doesn&apos;t support it!
+          </video>
+          <div className="m-auto -z-50 row-span-full col-span-full">
+            <Spiral />
+          </div>
+        </div>
         <h1 className="text-[2rem] md:text-[3.125rem] font-bold mb-1">Tanner Cecchetti</h1>
         <p className="text-[1.375rem]">Software engineer 👨‍💻</p>
         <p className="text-[1.375rem] mb-4">Seattle, WA 🌲</p>
